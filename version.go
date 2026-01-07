7059eba8553c999e8183013bfacdd27c9482dcab
Chris Szuttor <christhivarath@gmail.com>
2026-01-06 23:34:45 -0500

    chore: bump version to 1.1.5

--- version.go
+++ version.go
@@ -2,4 +2,4 @@ package main
 
 // Version is the application version, should match wails.json
 // This can be overridden at build time with: -ldflags "-X main.Version=1.2.3"
-var Version = "1.1.4"
+var Version = "1.1.5"
